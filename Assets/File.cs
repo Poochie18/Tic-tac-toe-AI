56c945920956123067f0ac28fffdc7197743efa4
Poochie <39067344+Poochie18@users.noreply.github.com>
2021-03-21 14:52:08 +0200

    v0.0.2
    AI add

--- Assets/File.cs
+++ Assets/File.cs
@@ -1,17 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class File : MonoBehaviour
 {
-
-    void Start()
-    {
-        string path = @"F:\Repository\Tic-tac-toe\Assets\text.txt";
-        var readText = System.IO.File.ReadAllText(path);
-        var result = new Dictionary<string, string>();
-
-
-    }
 
 }
--- Assets/GameLogic.cs
+++ Assets/GameLogic.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
-using System.Linq;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Globalization;
 
 public class GameLogic : MonoBehaviour
 {
@@ -15,7 +17,13 @@ public class GameLogic : MonoBehaviour
     public Button[] titles;
     public bool gamePause = false;
     public int[] markedTiles = new int[9];
+
     Dictionary<int, string> playersIcons = new Dictionary<int, string> { {1,"X" }, { 2, "O" } };
+    Dictionary<string, float> all = new Dictionary<string, float>();
+
+    List<string> AISteps = new List<string>();
+
+    private float alpfa = 0.1f;
 
     private void Start()
     {
@@ -26,8 +34,10 @@ public class GameLogic : MonoBehaviour
     {
         whichPlayer = 1;
         turnCount = 0;
-        whoseTurn.text = "X";
+        whoseTurn.text = playersIcons[1];
+        AISteps.Clear();
 
+        WritingFile();
         for(int i = 0; i < markedTiles.Length; i++)
         {
             markedTiles[i] = 0;
@@ -37,21 +47,25 @@ public class GameLogic : MonoBehaviour
 
     public void PressTheTile(int tileIndex)
     {
-        
-        var chosenTitle = titles[tileIndex];
-        
         turnCount++;
-        markedTiles[tileIndex] = whichPlayer;
+        ChangeStates(tileIndex);
+        
         if (turnCount > 4)
         {
             CheckingWinner();
         }
-        //string result = string.Join("", markedTiles);
-        ChangeStates(chosenTitle);
+
+        if (whichPlayer == 2 && !gamePause)
+        {
+            AITurn();
+        }
+
     }
 
-    void ChangeStates(Button tile)
+    void ChangeStates(int tileIndex)
     {
+        markedTiles[tileIndex] = whichPlayer;
+        var tile = titles[tileIndex];
         var localTile = tile.GetComponentInChildren<Text>();
         localTile.text = playersIcons[whichPlayer];
         tile.interactable = false;
@@ -60,6 +74,105 @@ public class GameLogic : MonoBehaviour
             whichPlayer = whichPlayer == 1 ? 2 : 1;
         }
         whoseTurn.text = playersIcons[whichPlayer];
+    }
+
+    void AITurn()
+    {  
+        MakePosibleMoves();       
+    }
+
+    void MakePosibleMoves()
+    {
+        List<string> posibleMoves = new List<string>();
+        string result = string.Join("", markedTiles);
+        for(int i = 0; i < result.Length; i++)
+        {
+            var temp = result;
+            if (result[i] == '0')
+            {
+                posibleMoves.Add(temp.Remove(i, 1).Insert(i, "2")) ;
+            }
+        }
+
+        string bestMove = "";
+        float bestScore = 0f;
+
+        foreach (KeyValuePair<string, float> keyValue in all)
+        {
+            foreach(var move in posibleMoves)
+            {
+                if (keyValue.Key == move)
+                {
+                    //Debug.Log($"{keyValue.Key} - {keyValue.Value}");
+                    if(bestScore < keyValue.Value)
+                    {
+                        bestScore = keyValue.Value;
+                        bestMove = keyValue.Key;
+                    }
+                    /*if (UnityEngine.Random.Range(0, 11) == 1)
+                    {
+                        bestScore = 100f;
+                    }*/
+                }
+            }  
+        }
+        //Debug.Log($"{bestMove} - {bestScore}");
+        AISteps.Add(bestMove);
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (bestMove[i] != '0' && markedTiles[i] == 0)
+            {
+                Debug.Log($"{i}");
+                ChangeStates(i);
+            }
+        }
+    }
+
+    void WritingFile()
+    {
+        string path = @"F:\Repository\Tic-tac-toe\Assets\text2.txt";
+
+        using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                all.Add(line.Split(' ')[0], Convert.ToSingle(line.Split(' ')[1]));
+            }
+        }
+    }
+
+    void CalculateNewWeights(int winner)
+    {
+        foreach (var keyValue in all)
+        {
+            foreach (var move in AISteps)
+            {
+                if(move == keyValue.Key)
+                {
+                    
+                    float newWeight = keyValue.Value + alpfa * (winner - keyValue.Value);
+                    all[keyValue.Key] = newWeight;
+                }
+            }
+        }
+        ReWriting();
+    }
+
+    void ReWriting()
+    {
+        Debug.Log("newWeight");
+        string path = @"F:\Repository\Tic-tac-toe\Assets\text2.txt";
+        FileStream file1 = new FileStream(path, FileMode.Create); //создаем файловый поток
+        StreamWriter writer = new StreamWriter(file1); //создаем «потоковый писатель» и связываем его с файловым потоком
+        foreach (var keyValue in all)
+        {
+            Debug.Log("Rewriting");
+            writer.Write(string.Join(" ", keyValue.Key, keyValue.Value));
+        }
+
+        Debug.Log("Rewriting Finish");     //записываем в файл
+        writer.Close(); //закрываем поток. Не закрыв поток, в файл ничего не запишется
     }
 
     void CheckingWinner()
@@ -74,7 +187,7 @@ public class GameLogic : MonoBehaviour
             markedTiles[1] == whichPlayer && markedTiles[4] == whichPlayer && markedTiles[7] == whichPlayer,
             markedTiles[2] == whichPlayer && markedTiles[5] == whichPlayer && markedTiles[8] == whichPlayer,
             markedTiles[0] == whichPlayer && markedTiles[4] == whichPlayer && markedTiles[8] == whichPlayer,
-            markedTiles[2] == whichPlayer && markedTiles[3] == whichPlayer && markedTiles[4] == whichPlayer,
+            markedTiles[2] == whichPlayer && markedTiles[4] == whichPlayer && markedTiles[6] == whichPlayer,
         };
         foreach(var win in winChecksList)
         {
@@ -83,6 +196,12 @@ public class GameLogic : MonoBehaviour
                 winner.text = "Winner";
                 Debug.Log($"{playersIcons[whichPlayer]} has won!");
                 gamePause = true;
+                CalculateNewWeights(whichPlayer - 1);
+                foreach (Button btn in titles)
+                {
+                    btn.interactable = false;
+                }
+                
             }
         }
     }
